7ede78ced016e3c5dcb472638e523607243431e2
caglarkoksal <caglarkoksal@users.noreply.github.com>
2023-04-26 21:06:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,15 +13,12 @@ tileLayer.addTo(map);
 // Make an HTTP request to the Overpass API
 var xmlhttp = new XMLHttpRequest();
 var url = "https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];(node['amenity'='pharmacy']({{bbox}});node['amenity'='library']({{bbox}});node['amenity'='post_office']({{bbox}});node['amenity'='bus_stop']({{bbox}});node['shop'='retail']({{bbox}});node['healthcare'='doctor']({{bbox}}););out body;>;out skel qt;";
-xmlhttp.onreadystatechange = function() {
-  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-    // Parse the GeoJSON data
+xmlhttp.onreadystatechange = () => {
+  if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
     var data = JSON.parse(xmlhttp.responseText);
-    // Create a Leaflet GeoJSON layer
-    var geoJsonLayer = L.geoJSON(data, {
-      // Define the point-to-layer function
-      pointToLayer: function(feature, latlng) {
-        // Create a custom marker icon based on the feature properties
+
+    geoJsonLayer = L.geoJSON(data, {
+      pointToLayer: (feature, latlng) => {
         var iconUrl = "icons/" + feature.properties.amenity + ".png";
         var iconSize = [32, 32];
         var iconAnchor = [16, 16];
@@ -30,26 +27,28 @@ xmlhttp.onreadystatechange = function() {
           iconSize: iconSize,
           iconAnchor: iconAnchor
         });
-        // Create a Leaflet marker with the custom icon
         var marker = L.marker(latlng, { icon: icon });
-        // Bind a popup to the marker with the feature properties
         marker.bindPopup(feature.properties.name);
-        // Return the marker to be added to the layer
         return marker;
       }
     });
-    // Add the GeoJSON layer to the map
     geoJsonLayer.addTo(map);
+  }
+};
 
-  // Add an event listener for when the user drops a pin
-map.on("click", function(e) {
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+var geoJsonLayer;
+
+map.on("click", (e) => {
   // Set the waypoint to the dropped pin location
   control.setWaypoints([
     L.latLng(e.latlng.lat, e.latlng.lng)
   ]);
 
   // Filter the GeoJSON layer to include only the amenities within the 10-minute walking distance
-  var amenities = geoJsonLayer.toGeoJSON().features.filter(function(feature) {
+  var amenities = geoJsonLayer.toGeoJSON().features.filter((feature) => {
     var distanceToAmenity = L.latLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0]).distanceTo(e.latlng);
     return distanceToAmenity <= 1000 * 10; // meters
   });
@@ -57,30 +56,29 @@ map.on("click", function(e) {
   // Display the number of accessible amenities within the 10-minute walking distance
   alert("You can access " + amenities.length + " amenities within a 10-minute walking distance.");
 
-  // Add a new GeoJSON layer with the filtered amenities
-  var filteredLayer = L.geoJSON(amenities, {
-    // Define the point-to-layer function
-    pointToLayer: function(feature, latlng) {
-      // Create a custom marker icon based on the feature properties
-      var iconUrl = "icons/" + feature.properties.amenity + ".png";
-      var iconSize = [32, 32];
-      var iconAnchor = [16, 16];
-      var icon = L.icon({
-        iconUrl: iconUrl,
-        iconSize: iconSize,
-        iconAnchor: iconAnchor
-      });
-      // Create a Leaflet marker with the custom icon
-      var marker = L.marker(latlng, { icon: icon });
-      // Bind a popup to the marker with the feature properties
-      marker.bindPopup(feature.properties.name);
-      // Return the marker to be added to the layer
-      return marker;
-    }
-  });
-
-  // Add the filtered GeoJSON layer to the map and remove the previous layer
-  map.removeLayer(geoJsonLayer);
-  filteredLayer.addTo(map);
-  geoJsonLayer = filteredLayer;
+// Add a new GeoJSON layer with the filtered amenities
+var filteredLayer = L.geoJSON(amenities, {
+  pointToLayer: function(feature, latlng) {
+    // Create a custom marker icon based on the feature properties
+    var iconUrl = "icons/" + feature.properties.amenity + ".png";
+    var iconSize = [32, 32];
+    var iconAnchor = [16, 16];
+    var icon = L.icon({
+      iconUrl: iconUrl,
+      iconSize: iconSize,
+      iconAnchor: iconAnchor
+    });
+    // Create a Leaflet marker with the custom icon
+    var marker = L.marker(latlng, { icon: icon });
+    // Bind a popup to the marker with the feature properties
+    marker.bindPopup(feature.properties.name);
+    // Return the marker to be added to the layer
+    return marker;
+  }
 });
+
+// Add the filtered GeoJSON layer to the map and remove the previous layer
+map.removeLayer(geoJsonLayer);
+filteredLayer.addTo(map);
+geoJsonLayer = filteredLayer;
+
